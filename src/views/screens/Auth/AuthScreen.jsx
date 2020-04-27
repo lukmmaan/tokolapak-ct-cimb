eb40c2d43fb12ddd1825e42b37ee33ce5fd2b655
lukmmaan <lyudokusumo@gmail.com>
2020-04-27 12:08:10 +0700

    27 April 2020 12:08PM

---
+++ src/views/screens/Auth/AuthScreen.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import TextField from "../../components/TextField/TextField"
+import ButtonUI from "../../components/Button/Button"
+
+class AuthScreen extends React.Component{
+    render(){
+        return(
+            <div className="container">
+                <div className="row mt-5">
+                    <div className="col-5">
+                        <h3>Log In</h3>
+                        <p className="mt-4">
+                            Welcome Back. 
+                            <br/>Please, Login to your account
+                        </p>
+                        <TextField placeholder="Username" className="mt-5"/>
+                        <TextField placeholder="Password" className="mt-2"/>
+                        <div className="d-flex justify-content-center">
+                        <ButtonUI type="contained" className="mt-4">
+                            Login
+                        </ButtonUI>
+                        </div>
+                    </div>
+                    <div className="col-7">Picture</div>
+                </div>
+            </div>
+        )
+    }
+}
+export default AuthScreen
