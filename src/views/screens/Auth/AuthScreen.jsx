92484b58009f4acc41cfb7bcd3feee7f0de192d1
lukmmaan <lyudokusumo@gmail.com>
2020-04-28 12:26:49 +0700

    gg

--- src/views/screens/Auth/AuthScreen.jsx
+++ src/views/screens/Auth/AuthScreen.jsx
@@ -1,15 +1,18 @@
-import React from "react"
-import { connect } from "react-redux"
-import { Redirect } from "react-router-dom"
-import Cookies from "universal-cookie"
-import { registerHandler, loginHandler } from "../../../redux/actions"
-import TextField from "../../components/TextField/TextField"
-import ButtonUI from "../../components/Button/Button"
-import "./AuthScreen.css"
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
+import Cookies from "universal-cookie";
+
+import TextField from "../../components/TextField/TextField";
+import ButtonUI from "../../components/Button/Button";
+import "./AuthScreen.css";
+
+// actions
+import { registerHandler, loginHandler } from "../../../redux/actions";
 
 class AuthScreen extends React.Component {
   state = {
-    kondisi: "register",
+    activePage: "register",
     loginForm: {
       username: "",
       password: "",
@@ -22,48 +25,68 @@ class AuthScreen extends React.Component {
       password: "",
       showPassword: false,
     },
-  }
+  };
 
   componentDidUpdate() {
     if (this.props.user.id) {
-      const cookie = new Cookies()
-      cookie.set("authData", JSON.stringify(this.props.user))
+      const cookie = new Cookies();
+      cookie.set("authData", JSON.stringify(this.props.user));
     }
   }
 
   inputHandler = (e, field, form) => {
-    const { value } = e.target
+    const { value } = e.target;
     this.setState({
       [form]: {
         ...this.state[form],
         [field]: value,
       },
-    })
-  }
+    });
 
-  fungsiRegis = () => {
-    const { username, fullName, password, email } = this.state.registerForm
-    let penggunaBaru = {username,fullName,password,email}
-    this.props.onRegister(penggunaBaru)
-  }
+    // this.setState({ loginForm: {
+    //   ...this.state.loginForm,
+    //   [fieldYangDiganti]: value
+    // }})
+  };
 
-  fungsiLogin = () => {
-    const { username, password } = this.state.loginForm
-    let penggunaBaru = {username,password}
-    this.props.onLogin(penggunaBaru)
-  }
-  fungsiCheckbox =(e,form)=>{
-    const {checked} = e.target
+  registerBtnHandler = () => {
+    const { username, fullName, password, email } = this.state.registerForm;
+    let newUser = {
+      username,
+      fullName,
+      password,
+      email,
+    };
+
+    this.props.onRegister(newUser);
+  };
+
+  loginBtnHandler = () => {
+    const { username, password } = this.state.loginForm;
+    let newUser = {
+      username,
+      password,
+    };
+
+    this.props.onLogin(newUser);
+  };
+
+  checkboxHandler = (e, form) => {
+    const { checked } = e.target;
+
+    console.log(checked);
+
     this.setState({
-      [form]:{
+      [form]: {
         ...this.state[form],
-        showPassword:checked
-      }
-    })
-  }
+        showPassword: checked,
+      },
+    });
+  };
+
   renderAuthComponent = () => {
-    const { kondisi } = this.state
-    if (kondisi == "register") {
+    const { activePage } = this.state;
+    if (activePage == "register") {
       return (
         <div className="mt-5">
           <h3>Register</h3>
@@ -93,20 +116,26 @@ class AuthScreen extends React.Component {
             onChange={(e) => this.inputHandler(e, "password", "registerForm")}
             placeholder="Password"
             className="mt-2"
-            type={this.state.registerForm.showPassword ? "text":"password"}
+            type={this.state.registerForm.showPassword ? "text" : "password"}
           />
-          <input onChange={e=>this.fungsiCheckbox(e,"registerForm")} type="checkbox" className="mt-3" name="showPasswordRegister"/>{" "} Show Password
+          <input
+            type="checkbox"
+            onChange={(e) => this.checkboxHandler(e, "registerForm")}
+            className="mt-3"
+            name="showPasswordRegister"
+          />{" "}
+          Show Password
           <div className="d-flex justify-content-center">
             <ButtonUI
               type="contained"
-              onClick={this.fungsiRegis}
+              onClick={this.registerBtnHandler}
               className="mt-4"
             >
               Register
             </ButtonUI>
           </div>
         </div>
-      )
+      );
     } else {
       return (
         <div className="mt-5">
@@ -129,7 +158,7 @@ class AuthScreen extends React.Component {
           />
           <div className="d-flex justify-content-center">
             <ButtonUI
-              onClick={this.fungsiLogin}
+              onClick={this.loginBtnHandler}
               type="contained"
               className="mt-4"
             >
@@ -137,13 +166,13 @@ class AuthScreen extends React.Component {
             </ButtonUI>
           </div>
         </div>
-      )
+      );
     }
-  }
+  };
 
   render() {
     if (this.props.user.id > 0) {
-      return <Redirect to="/" />
+      return <Redirect to="/" />;
     }
     return (
       <div className="container">
@@ -152,19 +181,19 @@ class AuthScreen extends React.Component {
             <div className="d-flex flex-row">
               <ButtonUI
                 className={`auth-screen-btn ${
-                  this.state.kondisi == "register" ? "active" : null
-                  }`}
+                  this.state.activePage == "register" ? "active" : null
+                }`}
                 type="outlined"
-                onClick={() => this.setState({ kondisi: "register" })}
+                onClick={() => this.setState({ activePage: "register" })}
               >
                 Register
               </ButtonUI>
               <ButtonUI
                 className={`ml-3 auth-screen-btn ${
-                  this.state.kondisi == "login" ? "active" : null
-                  }`}
+                  this.state.activePage == "login" ? "active" : null
+                }`}
                 type="outlined"
-                onClick={() => this.setState({ kondisi: "login" })}
+                onClick={() => this.setState({ activePage: "login" })}
               >
                 Login
               </ButtonUI>
@@ -179,19 +208,19 @@ class AuthScreen extends React.Component {
           <div className="col-7">Picture</div>
         </div>
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
     user: state.user,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = {
   onRegister: registerHandler,
   onLogin: loginHandler,
-}
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(AuthScreen)
+export default connect(mapStateToProps, mapDispatchToProps)(AuthScreen);
